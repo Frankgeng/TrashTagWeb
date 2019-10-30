f2a664d8efebd4cd1960bcf54149a954a7a89d77
Shelby <sb09745@georgiasouthern.edu>
2019-10-29 23:55:50 -0400

    added some components and layout/styles for the home page

--- trashtag/src/app/app.module.ts
+++ trashtag/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MapComponent } from './map/map.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    MapComponent
+    MapComponent,
+    HomeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
