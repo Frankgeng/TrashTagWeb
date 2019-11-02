aa86780ba6008c26339bb668a3884f6ddb2563c3
Major League Hacking <sb09745@georgiasouthern.edu>
2019-11-02 17:53:59 -0400

    fixed form to show to page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,23 +8,5 @@ import { ValidationService } from './login/validation.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  userForm: any;
-
-  constructor(private formBuilder: FormBuilder){
-    this.userForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, ValidationService.emailValidator]],
-      userName: ['', Validators.required],
-      password: ['', Validators.required],
-      location: ['', [Validators.required, Validators.maxLength(50)]],
-      time: ['', [Validators.required, Validators.maxLength(10)]],
-      eventName: ['', Validators.required]
-    });
-  }
-
-  saveUser() {
-    if(this.userForm.dirty && this.userForm.valid) {
-      alert(`${this.userForm.value.name}, thank you for your cleaning efforts!`);
-    }
-  }
+ 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ValidationService } from './validation.service';
+@NgModule({
+  imports: [FormsModule, ReactiveFormsModule]
+})
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,8 +12,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userForm: any;
 
+  constructor(private formBuilder: FormBuilder){
+    this.userForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, ValidationService.emailValidator]],
+      userName: ['', Validators.required],
+      password: ['', Validators.required],
+      location: ['', [Validators.required, Validators.maxLength(50)]],
+      time: ['', [Validators.required, Validators.maxLength(10)]],
+      eventName: ['', Validators.required]
+    });
+  }
+
+  saveUser() {
+    if(this.userForm.dirty && this.userForm.valid) {
+      alert("${this.userForm.value.name}, thank you for your cleaning efforts!");
+    }
+  }
   ngOnInit() {
   }
 
