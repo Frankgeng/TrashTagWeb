bff762aaea3dfa8989462d7b6b0cb8ef3ebbe879
Major League Hacking <sb09745@georgiasouthern.edu>
2019-11-02 17:14:01 -0400

    added form to login component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ValidationService } from './login/validation.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'trashtag';
+  userForm: any;
+
+  constructor(private formBuilder: FormBuilder){
+    this.userForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, ValidationService.emailValidator]],
+      userName: ['', Validators.required],
+      password: ['', Validators.required],
+      location: ['', [Validators.required, Validators.maxLength(50)]],
+      time: ['', [Validators.required, Validators.maxLength(10)]],
+      eventName: ['', Validators.required]
+    });
+  }
+
+  saveUser() {
+    if(this.userForm.dirty && this.userForm.valid) {
+      alert(`${this.userForm.value.name}, thank you for your cleaning efforts!`);
+    }
+  }
 }
